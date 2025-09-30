368948dfe9deb7c7a58368a0c2982e63158175f3
rakibch <rakib9204@gmail.com>
2025-09-30 10:59:30 +0600

    fabricbarcode model create

---
+++ app/Models/FabricBarCode.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class FabricBarCode extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['fabric_id','barcode_value','barcode_image_path','generated_at','generated_by'];
+
+    public function fabric()
+    {
+        return $this->belongsTo(Fabric::class);
+    }
+
+    public function generatedBy()
+    {
+        return $this->belongsTo(User::class,'generated_by');
+    }
+}
